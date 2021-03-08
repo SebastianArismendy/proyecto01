daa9da8cf1c87e51aa89485c91cad8aa58e38c21
Sebastian Arismendy <sebax1502@gmail.com>
2021-03-07 21:43:24 -0500

    Commit 01: se creo toda la estructura inicial del proyecto

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <title>Proyecto tile</title>
+    <link rel="stylesheet" content="text/css" href="css/estilos.css">
+    <link rel="stylesheet" content="text/css" href="css/normalize.css">
+</head>
+
+<body>
+ <div id="envoltura">
+    <header>
+        <h1>Programación; web &#10239;</h1>
+    </header>
+    <div id="main">
+        <p>En este Curso aprenderás a programar páginas web usando git.
+
+        <?php 
+            $curso = "Construcción de elementos de software web 2";
+            echo "<br/></br>ESTAS EN EL CURO: $curso"
+        ?>
+        </p>
+
+    </div>
+ </div>
+</body>
+</html>
