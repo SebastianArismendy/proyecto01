c353e0aad25f64b736340de78d7fa8cef3ce512a
Sebastian Arismendy <sebax1502@gmail.com>
2021-03-07 22:15:38 -0500

    Commit 02: se agrego una nueva página llamada página 2

---
+++ pagina2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <title>Proyecto tile</title>
+    <link rel="stylesheet" content="text/css" href="css/estilos.css">
+    <link rel="stylesheet" content="text/css" href="css/normalize.css">
+</head>
+
+<body>
+    <?php
+
+        $a = 12;
+        $b = 1000;
+
+        $a .= $b;
+
+        echo "<br /> Concatenado $a";
+
+    ?>
+</body>
+</html>
